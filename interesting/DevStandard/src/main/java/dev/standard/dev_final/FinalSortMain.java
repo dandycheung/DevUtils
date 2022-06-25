31541ca73319623118cc65f67ccc447309c951de
cjt <jtongttt@gmail.com>
2022-06-25 21:29:46 +0800

    refactor(DevStandard): 更新常量排序代码

--- interesting/DevStandard/src/main/java/dev/standard/dev_final/FinalSortMain.java
+++ interesting/DevStandard/src/main/java/dev/standard/dev_final/FinalSortMain.java
@@ -1,49 +1,34 @@
 package dev.standard.dev_final;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.text.Collator;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import dev.utils.DevFinal;
-import dev.utils.common.StringUtils;
+import dev.utils.common.FileIOUtils;
 
 /**
- * detail: DevFinal 常量排序
+ * detail: DevFinal.STR 常量格式化排序
  * @author Ttt
  */
 public class FinalSortMain {
 
     public static void main(String[] args) {
-        String              FORMAT = "public static final String %s = \"%s\";";
-        List<String>        lists  = new ArrayList<>();
-        Map<String, String> maps   = new HashMap<>();
-        Field[]             fields = FinalSortMain.class.getDeclaredFields();
-        for (Field field : fields) {
-            String descriptor = Modifier.toString(field.getModifiers());
-            descriptor = StringUtils.isEmpty(descriptor) ? "" : descriptor + " ";
-            maps.put(field.getName(), descriptor);
-
-            lists.add(field.getName());
-        }
-        // Collator 类是用来执行区分语言环境的 String 比较的, 这里选择使用 CHINA
-        Collator collator = Collator.getInstance(java.util.Locale.CHINA);
-        // 使根据指定比较器产生的顺序对指定对象数组进行排序
-        Collections.sort(lists, collator);
-
+        List<String>        sortList  = new ArrayList<>();
+        Map<String, String> resultMap = new HashMap<>();
+        // 拼接结果数据
         StringBuilder builder = new StringBuilder();
-        for (String key : lists) {
-            builder.append(DevFinal.SYMBOL.NEW_LINE)
-                    .append(String.format(FORMAT, key.toUpperCase(), key.toLowerCase()));
+        // 读取待格式化文件内容
+        List<String> texts = FileIOUtils.readFileToList(Utils.getFormatFilePath());
+        for (String text : texts) {
+            if (!text.contains(Utils.PSFS)) {
+                Utils.sortAppend(builder, sortList, resultMap);
+                builder.append(text).append(DevFinal.SYMBOL.NEW_LINE);
+            } else {
+                Utils.convertPSFS(text, sortList, resultMap);
+            }
         }
-        // 最终内容
-        String content = builder.toString();
-        System.out.println(content);
-
-        String debug = "";
+        Utils.saveFileByResultTxt(builder);
     }
 }
--- interesting/DevStandard/src/main/java/dev/standard/dev_final/Utils.java
+++ interesting/DevStandard/src/main/java/dev/standard/dev_final/Utils.java
@@ -2,70 +2,29 @@ package dev.standard.dev_final;
 
 import java.io.File;
 import java.text.Collator;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import dev.utils.DevFinal;
-import dev.utils.common.FileIOUtils;
 import dev.utils.common.FileUtils;
 import dev.utils.common.StringUtils;
 
 /**
- * detail: DevFinal.STR 常量格式化排序
+ * detail: DevFinal 字符串常量排序工具类
  * @author Ttt
  */
-public class FormatFileFinalSortMain {
+final class Utils {
 
-    public static void main(String[] args) {
-        List<String>        sortList  = new ArrayList<>();
-        Map<String, String> resultMap = new HashMap<>();
-        // 拼接结果数据
-        StringBuilder builder = new StringBuilder();
-        // 读取待格式化文件内容
-        List<String> texts = FileIOUtils.readFileToList(getFormatFilePath());
-        for (String text : texts) {
-            if (!text.contains(PSFS)) {
-                sortAppend(builder, sortList, resultMap);
-                builder.append(text).append(DevFinal.SYMBOL.NEW_LINE);
-            } else {
-                convertPSFS(text, sortList, resultMap);
-            }
-        }
-        String content = builder.toString();
-        // 移除最后一行换行符
-        content = StringUtils.clearEndsWith(
-                content, DevFinal.SYMBOL.NEW_LINE
-        );
-        boolean result = FileUtils.saveFile(
-                getResultFilePath(),
-                content.getBytes()
-        );
-        System.out.println("保存结果: " + result);
+    private Utils() {
     }
 
-    // ==========
-    // = 内部方法 =
-    // ==========
-
-    private static final String PSFS            = "public static final String ";
-    private static final String PSFS_LOWER_CASE = PSFS.toLowerCase();
-
-    private static String getPackagePath() {
-        return new File("/interesting/DevStandard/src/main/java/dev/standard/dev_final").getAbsolutePath();
-    }
-
-    private static String getFormatFilePath() {
-        return new File(getPackagePath(), "format.txt").getAbsolutePath();
-    }
-
-    private static String getResultFilePath() {
-        return new File(getPackagePath(), "result.txt").getAbsolutePath();
-    }
-
-    // =
+    public static final String PSFS            = "public static final String ";
+    public static final String PSFS_LOWER_CASE = PSFS.toLowerCase();
+    // 起点标记
+    public static final String START_TAG       = "public static final class STR {";
+    // 结尾标记
+    public static final String END_TAG         = " }";
 
     /**
      * 转换 public static final String 处理
@@ -73,7 +32,7 @@ public class FormatFileFinalSortMain {
      * @param sortList  待排序常量名
      * @param resultMap 带存储结果
      */
-    private static void convertPSFS(
+    public static void convertPSFS(
             final String original,
             final List<String> sortList,
             final Map<String, String> resultMap
@@ -107,7 +66,7 @@ public class FormatFileFinalSortMain {
      * @param sortList  待排序常量名
      * @param resultMap 带存储结果
      */
-    private static void sortAppend(
+    public static void sortAppend(
             final StringBuilder builder,
             final List<String> sortList,
             final Map<String, String> resultMap
@@ -125,5 +84,59 @@ public class FormatFileFinalSortMain {
         }
         sortList.clear();
         resultMap.clear();
+    }
+
+    // ==========
+    // = 保存结果 =
+    // ==========
+
+    /**
+     * 保存文件到 result.txt
+     * @param builder StringBuilder
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean saveFileByResultTxt(final StringBuilder builder) {
+        return saveFile(builder, getResultFilePath());
+    }
+
+    /**
+     * 保存文件
+     * @param builder  StringBuilder
+     * @param filePath 文件路径
+     * @return {@code true} success, {@code false} fail
+     */
+    public static boolean saveFile(
+            final StringBuilder builder,
+            final String filePath
+    ) {
+        String content = builder.toString();
+        // 移除最后一行换行符
+        content = StringUtils.clearEndsWith(
+                content, DevFinal.SYMBOL.NEW_LINE
+        );
+        boolean result = FileUtils.saveFile(
+                filePath, content.getBytes()
+        );
+        System.out.println("保存结果: " + result);
+        return result;
+    }
+
+    // ==========
+    // = 路径相关 =
+    // ==========
+
+    private static String getPackagePath() {
+        return new File(
+                System.getProperty("user.dir"),
+                "/interesting/DevStandard/src/main/java/dev/standard/dev_final"
+        ).getAbsolutePath();
+    }
+
+    public static String getFormatFilePath() {
+        return new File(getPackagePath(), "format.txt").getAbsolutePath();
+    }
+
+    public static String getResultFilePath() {
+        return new File(getPackagePath(), "result.txt").getAbsolutePath();
     }
 }
